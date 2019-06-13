ad5d68855ab0916f88a75e677de65a7d3f8ab75e
Adam Pavlicek <adamhpavlicek@gmail.com>
2019-06-13 16:55:02 -0500

    add insertBefore and insert after

--- linkd-list.js
+++ linkd-list.js
@@ -27,6 +27,37 @@ class LinkedList {
     }
   }
 
+  insertBefore(item, item2){
+    if(this.head === null){
+      this.insertFirst(item);
+    }else {
+      let tempNode = this.head;
+      while(tempNode.next.value !== item2){
+        tempNode = tempNode.next;
+      }
+      tempNode.next = new _Node(item, item2);
+    }
+  }
+
+  insertAfter(item, item2){
+    if(this.head === null){
+      this.insertFirst(item);
+    }else {
+      let tempNode = this.head;
+      while(tempNode.next.value !==item2){
+        tempNode = tempNode.next;
+      }
+      tempNode.next = new _Node(item2, item);
+    }
+  }
+
+  insertAt(item, pos){
+    if(this.head === null){
+      this.insertFirst(item);
+    }
+
+  }
+
   find(item){
     let currNode = this.head;
     if(!this.head){
@@ -71,3 +102,16 @@ class LinkedList {
   }
 }
 
+function main(){
+  let SLL = new LinkedList();
+  SLL.insertLast('Apollo');
+  SLL.insertLast('Boomer');
+  SLL.insertLast('Helo');
+  SLL.insertLast('Husker');
+  SLL.insertLast('starbuck');
+  SLL.insertLast('Tauhida');
+  SLL.insertAfter('cheese', 'Boomer');
+  return SLL;
+}
+
+console.log(main());
